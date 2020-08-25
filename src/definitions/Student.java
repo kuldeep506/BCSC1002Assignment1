0162e7870f1cf61c460e18d49908e62bcdbad502
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-08-25 18:42:27 +0530

    class FrontDesk.java created

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -10,10 +10,10 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Student {
-private String studentFullName;
-private long universityRollNumber;
-private int numberOfBooksIssuedToStudent;
-private Book[] nameOfBooksIssuedToStudent;
+    private String studentFullName;
+    private long universityRollNumber;
+    private int numberOfBooksIssuedToStudent;
+    private Book[] nameOfBooksIssuedToStudent;
 
     public Student(String studentFullName, long universityRollNumber, int numberOfBooksIssuedToStudent, Book[] nameOfBooksIssuedToStudent) {
         this.studentFullName = studentFullName;
