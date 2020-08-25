429301d0f6e60c3c77ad049fbeefb03a7e319212
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-08-25 19:06:25 +0530

    added menu using print statements in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -8,6 +8,8 @@ package execution;
 
 import definitions.Student;
 
+import java.util.Scanner;
+
 public class FrontDesk {
     private Student currentStudentDetails;
     private static final int ISSUE_NEW_BOOK=1;
@@ -15,6 +17,15 @@ public class FrontDesk {
     private static final int SHOW_ALL_ISSUED_BOOKS=3;
     private static final int EXIT=3;
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+        String currentStudentName;
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.\n");
+            System.out.println("3. Show me all my issues books.\n");
+            System.out.println("4. Exit.\n");
+        }
     }
 }
