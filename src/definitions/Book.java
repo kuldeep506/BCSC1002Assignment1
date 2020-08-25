55ae7cb6735be84becb3fc36b8148f869ccf1e54
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-08-25 18:05:30 +0530

    non-empty contructor created

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,11 @@ public class Book {
 private String bookName;
 private String authorName;
 private String ISBNNumber;
+
+    public Book(String bookName, String authorName, String ISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumber = ISBNNumber;
+    }
+
 }
